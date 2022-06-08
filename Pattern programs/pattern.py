35afddfb5247ec9be6dde5b4f44476e9024b12c7
Sagarika Hembram <76590161+sagarikah@users.noreply.github.com>
2022-06-08 14:02:36 +0530

    Add files via upload

---
+++ Pattern programs/pattern.py
@@ -0,0 +1,12 @@
+##Sample Input
+##5
+##
+##Sample Output
+##1
+##22
+##333
+##4444
+
+
+for i in range(1,int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(str(i*i))
